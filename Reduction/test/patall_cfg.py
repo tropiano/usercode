db08bb82207a17c7030b84e631452decf056ea74
lenzip <>
2009-03-06 08:37:22 +0000

    fixed pat config file

--- Reduction/test/patall_cfg.py
+++ Reduction/test/patall_cfg.py
@@ -20,6 +20,15 @@ process.source = cms.Source("PoolSource",
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
+####REREconstruct gen jets
+import PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi
+process.load('Firenze.JetUtilities.NewGenJetParticles_cff')
+##exclude ALL particles coming from status 3 leptons
+process.newGenParticlesForJets.checkForStatus3Mother.excludeSameFlavourDaughterOnly=cms.bool(False)
+process.load('RecoJets.Configuration.RecoGenJets_cff')
+process.sisCone5GenJets.src=cms.InputTag('newGenParticlesForJets')
+process.genjets = cms.Sequence(process.newGenParticlesForJets*process.sisCone5GenJets)
+
 
 # PAT Layer 0+1
 process.load("PhysicsTools.PatAlgos.patLayer0_cff")
@@ -35,13 +44,26 @@ from PhysicsTools.PatAlgos.tools.jetTools import *
 ##Load the right jet corrections
 #process.load('JetMETCorrections.Configuration.L2L3Corrections_Summer08_cff')
 ## ==== Example with CaloJets
-switchJetCollection(process, 
-        'sisCone5CaloJets',    # Jet collection; must be already in the event when patLayer0 sequence is executed
+## ==== Example with CaloJets
+switchJetCollection(process,
+        'sisCone5CaloJets',   # Jet collection; must be already in the event when patLayer0 sequence is executed
+        layers=[0,1],          # If you're not runnint patLayer1, set 'layers=[0]'
+        runCleaner="CaloJet", # =None if not to clean
         doJTA=True,            # Run Jet-Track association & JetCharge
         doBTagging=True,       # Run b-tagging
-        jetCorrLabel=('SC5','Calo'), # example jet correction name; set to None for no JEC
-        doType1MET=True,       # recompute Type1 MET using these jets
-        genJetCollection=cms.InputTag("sisCone5GenJets")) 
+        jetCorrLabel=None,     # Turn this off, do it by hand. This tool doesn't work with new JEC
+        doType1MET=False)      # recompute Type1 MET using these jets
+
+# now set JEC by hand
+process.jetCorrFactors.jetSource = cms.InputTag("sisCone5CaloJets")
+process.jetCorrFactors.L1Offset  = cms.string('none')
+process.jetCorrFactors.L2Relative= cms.string('Summer08_L2Relative_SC5Calo')
+process.jetCorrFactors.L3Absolute= cms.string('Summer08_L3Absolute_SC5Calo')
+process.jetCorrFactors.L4EMF     = cms.string('none')
+process.jetCorrFactors.L5Flavor  = cms.string('none')
+process.jetCorrFactors.L6UE      = cms.string('none')
+process.jetCorrFactors.L7Parton  = cms.string('none')
+
 
 #manually change the jet collection to be used in the genJetMatching        
 process.jetGenJetMatch.matched=cms.InputTag('sisCone5GenJets')
@@ -50,8 +72,9 @@ process.patAODJetTracksAssociator.tracks = cms.InputTag('sisCone5JetTracksAssoci
 
 #process.content = cms.EDAnalyzer("EventContentAnalyzer")
 process.p = cms.Path(
+                process.genjets
                 ## process.iterativeCone5BasicJets +  ## Turn on this to run tests on BasicJets
-                process.patLayer0  
+                + process.patLayer0  
                 #process.patLayer0_patTuple_withoutPFTau  
                 #+ process.content    # uncomment to get a dump 
                 + process.patLayer1
@@ -70,7 +93,7 @@ process.outpath = cms.EndPath(process.out)
 process.load("PhysicsTools.PatAlgos.patLayer1_EventContent_cff")
 process.out.outputCommands.extend(process.patLayer1EventContent.outputCommands)
 sisConeEventContent = cms.PSet(
-  outputCommands = cms.untracked.vstring('keep *_sisCone5GenJets_*_*')
+  outputCommands = cms.untracked.vstring('keep *_sisCone5GenJets_*_PAT')
 )
 process.out.outputCommands.extend(sisConeEventContent.outputCommands)
 ## Necessary fixes to run 2.2.X on 2.1.X data
