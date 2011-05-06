a60a4aebc3a4eaabc8f67adbf3f12a3b8526d289
sfrosali <>
2011-05-06 09:42:56 +0000

    Code updated for 397

--- PATAnalysis/bin/zeejets.cc
+++ PATAnalysis/bin/zeejets.cc
@@ -167,7 +167,7 @@ int PreDefName, ProcEvents;
   bool Log = false;
   
   //Path of PATAnalysis dir - DO NOT FORGET THE SLASH AT THE END OF THE PATH
-  string path="/data/sfrosali/Zjets/Commit/CMSSW_3_8_7/src/Firenze/PATAnalysis/bin";
+  string path="/data/sfrosali/Zjets/CMSSW_3_9_7/src/Firenze/PATAnalysis/bin";
   
   if(sample=="data"){
   GenParticleMatch = "False";
