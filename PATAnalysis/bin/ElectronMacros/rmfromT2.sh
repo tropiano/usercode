104b2fb74d2f48672d474d89099c3852312161dd
sandro <>
2012-09-10 13:35:34 +0000

    macro rmfromT2.sh modified

--- PATAnalysis/bin/ElectronMacros/rmfromT2.sh
+++ PATAnalysis/bin/ElectronMacros/rmfromT2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 #to set: TIER = 0: rm @ CERN; TIER =1: rm @ PISA 
-TIER=1
+TIER=0
 
 LIST_RM=list_rm.txt
 PATH_PISA=srm://stormfe1.pi.infn.it:8444/srm/managerv2?SFN=/
@@ -10,10 +10,12 @@ PATH_CERN=srm://srm-eoscms.cern.ch:8443/srm/v2/server?SFN=/
 USER_PISA=/cms/store/user/sandro/
 USER_CERN=/eos/cms/store/user/sandro/
 
-DIR=SandroGonziCrabTest/SandroGonziCrabTest/07b5bb36faa323ae4fefd73d7a1ce628/Directory/
+DIR=Test/
 
 EXIT_SUCCESS=1
 EXIT_FAILURE=0
+
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
 
 if [ $TIER != 0 -a $TIER != 1 ]; then
 
@@ -24,7 +26,7 @@ fi
 
 if [ $TIER -eq 0 ]; then
 
-	PATHDIR=$PATH_CERN
+	PATHTIER=$PATH_CERN
 	USER=$USER_CERN
 
 	/afs/cern.ch/project/eos/installation/pro/bin/eos.select ls -l $USER$DIR > $LIST_RM
@@ -34,41 +36,44 @@ if [ $TIER -eq 0 ]; then
 
 	for i in `gawk '{print $9}' $LIST_RM`; do
 #	for i in `gawk '{print $5}' $LIST_RM`; do # for cmsLs
-		echo "File number " $COUNT
-		echo $i "Removing"
-		COUNT=`expr $COUNT + 1`;
+		if [ "$PATHTIER$USER$DIR$i" != "PATHTIER$USER$DIR" ]; then
+			echo "File number " $COUNT
+			echo $i "Removing"
+			COUNT=`expr $COUNT + 1`;
 
-		srmrm -2 "$PATHDIR$USER$DIR$i"
+			srmrm -2 "$PATHTIER$USER$DIR$i"
+		fi
 	done
 	
 	echo "..." 
-	echo "Directory $PATHDIR$USER$DIR Removing"
-	srmrmdir -2 "$PATHDIR$USER$DIR"
+	echo "Directory $PATHTIER$USER$DIR Removing"
+	srmrmdir -2 "$PATHTIER$USER$DIR"
 
 	exit $EXIT_SUCCESS
 fi
 
 if [ $TIER -eq 1 ]; then
 
-	PATHDIR=$PATH_PISA
+	PATHTIER=$PATH_PISA
 	USER=$USER_PISA
 
-	lcg-ls -l -b -D srmv2 "$PATHDIR$USER$DIR" > $LIST_RM
+	lcg-ls -l -b -D srmv2 "$PATHTIER$USER$DIR" > $LIST_RM
 
 	COUNT=1
 
 	for i in `gawk '{print $7}' $LIST_RM`; do
-		echo "File number " $COUNT
-		echo $i "Removing"
-		COUNT=`expr $COUNT + 1`;
+		if [ "$PATHTIER$USER$DIR" != "$PATHTIER$i/" ]; then
+			echo "File number " $COUNT
+			echo $i "Removing"
+			COUNT=`expr $COUNT + 1`;
 
-		srmrm -2 "$PATHDIR$i"
-
+			srmrm -2 "$PATHTIER$i"
+		fi
 	done
-	
+
 	echo "..." 
-	echo "Directory $PATHDIR$USER$DIR Removing"
-	srmrmdir -2 "$PATHDIR$USER$DIR"
+	echo "Directory $PATHTIER$USER$DIR Removing"
+	srmrmdir -2 "$PATHTIER$USER$DIR"
 
 
 	exit $EXIT_SUCCESS
