776e30a3a60c32aa9ad134115d79f418c8d56c4e
sandro <>
2012-11-15 16:07:42 +0000

    Electron-Photon TeP

--- PATAnalysis/bin/zeetp.cc
+++ PATAnalysis/bin/zeetp.cc
@@ -15,19 +15,25 @@ int main() {
 
 	//Input files - Z2
 	string training_sign = 
-	"/raid/sandro/Analisi/rel_CMSSW_4_2_5_modifiche_TeP/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/DYJetstoLL_All_Train.root";
+	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/DYJetsToLL_M50_Train_TeP_ElePhoton.root";
 	string training_back = 
-	"/raid/sandro/Analisi/rel_CMSSW_4_2_5_modifiche_TeP/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/Background_Train.root";
+	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/Background_Train_TeP_ElePhoton.root";
 	string total = 
-	"/raid/sandro/Analisi/rel_CMSSW_4_2_5_modifiche_TeP/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/MC-Data_Sample.root"; 
+	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/DYJetsToLL_M50_Sample_TeP_ElePhoton.root"; 
+//	"Background_Sample_TeP_ElePhoton.root"; 
+//	"Data_TeP_ElePhoton.root"; 
+
 
 	//Output files
 	string output_train_sign =
-	"/raid/sandro/Analisi/rel_CMSSW_4_2_5_modifiche_TeP/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/TrainSig_JetPt15_Incl_Global.root";
+	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/SigTrain_Pt_Barrel.root";
 	string output_train_back =
-	"/raid/sandro/Analisi/rel_CMSSW_4_2_5_modifiche_TeP/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/TrainBack_JetPt15_Incl_Global.root";
+	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/BackTrain_Pt_Barrel.root";
 	string output_total =
-	"/raid/sandro/Analisi/rel_CMSSW_4_2_5_modifiche_TeP/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/TESTPlot_DATA_JetPt15_Incl_Global.root";
+	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/SigSample_Pt_Barrel.root";
+//	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/BackSample_Pt_Barrel.root";
+//	"/gpfs/ddn/cms/user/gonzi/Analisi/rel_CMSSW_4_2_5_PhotonsPAT/CMSSW_4_2_5/src/Firenze/PATAnalysis/bin/AnalysisElectrons/Data_Pt_Barrel.root";
+
 
 	//Selections used
 	string selections = "SYM";
@@ -36,26 +42,27 @@ int main() {
 	string cut = "Global";
 
 	//Multiplicity - "excl" = Exclusive; "incl" = Inclusive
-	string mult = "incl";
+	//Topology - "barrel" = Barrel; "endcap" = Endcap;
+	string TPdir = "barrel";
 
 	//Analyzer steps
-	bool tr_sig = false;
-	bool tr_back = false;
+	bool tr_sig = true;
+	bool tr_back = true;
 	bool total_an = true;
 
 	//Mass bin
-	int massbin = 5;
+	int massbin = 3;
 
 	//IF directories are different from default ones
 	string nodef_dir = "";
 	string nodef_secdir = "";
 
 	//Cuts
-	int _Acc = 1;
-	int _Trg = 4;
+	int _Acc =  1;
+	int _Trg =  0;
 	int _Qual = 0;
-	int _Imp = 0;
-	int _Iso = 2;
+	int _Imp =  0;
+	int _Iso =  2;
 	int _ElID = 3;
 	int _Conv = 0;
 	
@@ -104,23 +111,33 @@ int main() {
 	string dir = "EfficiencyElectron/";
 	string sec_el_dir = "EfficiencyElectron/";
 
-	string dataset = "datasetElectron";
+	string dataset = "datasetelectron";
 	
 
-	if(selections=="SYM" && mult=="excl") dir+="Tag&Probe_Excl";
+	if(selections=="SYM" && TPdir=="excl") dir+="Tag&Probe_Excl";
+	if(selections=="SYM" && TPdir=="incl") dir+="Tag&Probe_Incl";
 
-	if(selections=="SYM" && mult=="incl") dir+="Tag&Probe_Incl";
-
-	if(selections=="ASYM" && mult=="excl"){
+	if(selections=="ASYM" && TPdir=="excl"){
 		dir+="Tag&Probe0_Excl";
 		sec_el_dir+="Tag&Probe1_Excl";
 	}
-
-	if(selections=="ASYM" && mult=="incl"){
+	if(selections=="ASYM" && TPdir=="incl"){
 		dir+="Tag&Probe0_Incl";
 		sec_el_dir+="Tag&Probe1_Incl";
 	}
 
+	if(selections=="SYM" && TPdir=="barrel") dir+="Tag&Probe_Pt_Barrel";
+	if(selections=="SYM" && TPdir=="endcap") dir+="Tag&Probe_Pt_Endcap";
+
+	if(selections=="ASYM" && TPdir=="barrel"){
+		dir+="Tag&Probe0_Pt_Barrel";
+		sec_el_dir+="Tag&Probe1_Pt_Barrel";
+	}
+	if(selections=="ASYM" && TPdir=="endcap"){
+		dir+="Tag&Probe0_Pt_Endcap";
+		sec_el_dir+="Tag&Probe1_Pt_Endcap";
+	}
+	
 	if(cut!="Global" && cut!="Double"){	
 		int ncut = 1;	
 		while(cut!=_RecoCutFlags[ncut]){
@@ -130,12 +147,10 @@ int main() {
 			ncut++;
 			if(ncut>6)throw cms::Exception("PATAnalysis:zeetpana_CutNotFound") << "ERROR! Cut selected not found!";
 		}
-
-		dir+=_RecoCutFlags[ncut].c_str();
-		sec_el_dir+=_RecoCutFlags[ncut].c_str();
-		dataset+=_RecoCutFlags[ncut].c_str();
-		dataset+="_";
-
+	  	dir+=_RecoCutFlags[ncut].c_str();
+	  	sec_el_dir+=_RecoCutFlags[ncut].c_str();
+	  	dataset+=_RecoCutFlags[ncut].c_str();
+	  	dataset+="_";
 	}
 
 	else if(cut=="Global"){
@@ -149,11 +164,12 @@ int main() {
 		sec_el_dir+="_Double";
 		dataset+="_Double_";
 	}
-	
 
 	if(nodef_dir!="") dir=nodef_dir;
 	if(nodef_secdir!="") sec_el_dir=nodef_secdir;
 
+	if(selections=="SYM") sec_el_dir="";
+	
 	EfficiencyAnalyzerElectron TP_TrainSign(train_sign, out_train_sign, dir.c_str(), sec_el_dir.c_str(), dataset.c_str());
 	EfficiencyAnalyzerElectron TP_TrainBack(train_back, out_train_back, dir.c_str(), sec_el_dir.c_str(), dataset.c_str()); 
 	EfficiencyAnalyzerElectron TP_Total(total_in, total_out, dir.c_str(), sec_el_dir.c_str(), dataset.c_str(), out_train_sign, out_train_back);
